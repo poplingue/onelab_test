b6866d4aeb6f7b238f440e960aad130ec5444b36
z <zizou_vb@hotmail.com>
2018-01-25 11:53:41 +0100

    my solution

--- model/User.py
+++ model/User.py
@@ -1,4 +1,32 @@
-class User:
 
-    def __init__(self):
-        pass
+
+from onelab_test import db
+
+class User(db.Model):
+    __tablename__ = "users"
+    firstname = db.Column(db.String(40), unique=False)
+    lastname =  db.Column(db.String(40), unique=False)
+    email = db.Column(db.String(120), unique=True, primary_key=True)
+
+    def __init__(self, firstname, lastname, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+
+    def __repr__(self):
+        return '{} {} {}'.format(self.firstname, self.lastname, self.email)
+
+    def query_all(self):
+    	return User.query.all()
+
+    def query_by_email(self, email):
+        return User.query.filter(User.email == email)
+
+    def modify_firstname_lastname(self, email, new_firstname, new_lastname):
+    	user = User.query.filter(User.email == email)
+    	user.firstname = new_firstname
+    	user.lastname = new_lastname
+    	db.session.commit()
+
+
+    
--- onelab_test.py
+++ onelab_test.py
@@ -3,11 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/my_postgres_db'
+db = SQLAlchemy(app)
+
+from model import User
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('GET/api/users', methods=['GET'])
+def query_all_user():
+	User.query_all()
+
+@app.route('GET/api/users/<email>', methods=['GET'])
+def query_user_by_email():
+	User.query_by_email()
+
+@app.route('GET/api/users/<email>', methods=['PUT'])
+def query_put():
+	User.modify_firstname_lastname()
 
 if __name__ == '__main__':
     app.run()
